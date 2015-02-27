111640a232f45bf2e97942215e855c509bf3b3de
bjlapp <bjlapp33@gmail.com>
2015-02-27 15:25:39 -0600

    add method in DLinkedList class

--- DLinkedList.java
+++ DLinkedList.java
@@ -2,9 +2,22 @@ package p2;
 
 public class DLinkedList<E> implements ListADT<E>{
 
+	private Listnode<E> head = null;
+	private Listnode<E> tail = null;
+ 	
 	@Override
 	public void add(E item) {
+		Listnode<E> temp;
 		// TODO Auto-generated method stub
+		if(head.equals(null)){
+			new Listnode<E>(item);
+			head.equals(item);
+			tail.equals(item);
+		}else{
+			temp = new Listnode<E>(item, null, tail);
+			tail.setNext(temp);
+			tail = temp;
+		}
 		
 	}
 
@@ -41,6 +54,7 @@ public class DLinkedList<E> implements ListADT<E>{
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
