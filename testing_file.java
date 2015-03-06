6a91f4b32ddf610451da5b7e7ca0688e7ebff60b
AdamConverse <adamconverse@dyn-72-33-245-0.uwnet.wisc.edu>
2015-03-06 14:55:17 -0600

    Revert "Read me file"
    This reverts commit 4cddd928ead3635eeb37d712a84507264254e230.

--- testing_file.java
+++ testing_file.java
@@ -4,17 +4,14 @@ public class testing_file{
 	public static void main (String[] args){
 		DLinkedList<Integer> list = new DLinkedList<Integer>();
 		
-		/*list.add(10);
+		list.add(10);
 		list.add(5);
 		list.add(1);
 		
 		list.add(1, 7);
-				*/
-		System.out.println(list.isEmpty());
 		
 		for(int i=0; i<list.size(); i++){
 			System.out.println(list.get(i));
 		}
-		
 	}
 }
