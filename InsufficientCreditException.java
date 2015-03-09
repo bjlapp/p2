8f0f911468fdc08d02bac48f5bf21930d157e3c5
AdamConverse <adamconverse@Adams-MacBook-Pro.local>
2015-03-08 21:36:57 -0500

    Addition to last commit

--- InsufficientCreditException.java
+++ InsufficientCreditException.java
@@ -1,5 +1,27 @@
-package p2;
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  AmazonStore.java
+// File:             InsufficentCreditException.java
+// Semester:         CS302 Spring 2015
+//
+// Author:           Adam Converse
+// Email:            acconverse@wisc.edu
+// CS Login:         adam
+// Lecturer's Name:  Jim Skrentny
+// Lab Section:      105
+//
+// Pair Partner:     Brandon Lapp
+// Email:            blapp@wisc.edu
+// CS Login:         brendon
+// Lecturer's Name:  Jim Skrentny
+// Lab Section:      105
 
+//package p2;
+
+/**
+ * Exception thrown when user does not have enough credit to buy an item.
+ *
+ */
 public class InsufficientCreditException extends Exception {
 	public InsufficientCreditException(){
 		
